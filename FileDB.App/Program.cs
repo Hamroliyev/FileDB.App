cc1182ec402d9cc5db94b867e881b993e59790ab
Kroos <hamroliyevahmadjon1909@gmail.com>
2024-04-13 06:11:28 +0500

    Calculate Total Size of Files.

--- FileDB.App/Program.cs
+++ FileDB.App/Program.cs
@@ -1,11 +1,11 @@
 ﻿using FileDB.App.Brokers.Loggings;
 using FileDB.App.Brokers.Storages;
-using FileDB.App.Models.Users;
 using FileDB.App.Services.Identities;
 using FileDB.App.Services.UserProcessings;
 using FileDB.App.Services.Users;
 using System;
-using System.Xml.Linq;
+using System.IO;
+using System.Reflection;
 
 namespace FileDB.App
 {
@@ -13,67 +13,129 @@ namespace FileDB.App
     {
         private static void Main(string[] args)
         {
-            string userChoice;
-            UserProcessingService userProcessingService = RegisterUserProcessingService();
+            string docPath = "../../../Assets/";
+            DirectoryInfo root = new DirectoryInfo(docPath);
+            long totalSize = 0;
 
-            do
+            try
             {
-                PrintMenu();
-                Console.Write("Enter your choice:");
-                userChoice = Console.ReadLine();
-                switch (userChoice)
+                foreach (FileInfo fileInfo in root.EnumerateFiles())
                 {
-                    case "1":
-                        Console.Clear();
-                        Console.Write("Enter you name:");
-                        string userName = Console.ReadLine();
-                        User user = new User() { Name = userName };
-                        userProcessingService.CreateNewUser(user);
-                        break;
-
-                    case "2":
+                    try
+                    {
+                        if (fileInfo.Length >= 0)
                         {
-                            Console.Clear();
-                            userProcessingService.DisplayUsers();
+                            Console.WriteLine($"{fileInfo.FullName}\t\t{fileInfo.Length.ToString("N0")}");
+                            totalSize = totalSize + fileInfo.Length;
                         }
-                        break;
-
-                    case "3":
-                        {
-                            Console.Clear();
-                            Console.WriteLine("Enter an id which you want to delete");
-                            Console.Write("Enter id:");
-                            string deleteWithIdStr = Console.ReadLine();
-                            int deleteWithId = Convert.ToInt32(deleteWithIdStr);
-                            userProcessingService.DeleteUser(deleteWithId);
-                        }
-                        break;
-
-                    case "4":
-                        {
-                            Console.Clear();
-                            Console.WriteLine("Enter an id which you want  to edit");
-                            Console.Write("Enter an id:");
-                            string idStr = Console.ReadLine();
-                            int id = Convert.ToInt32(idStr);
-                            Console.Write("Enter name:");
-                            string name = Console.ReadLine();
-                            User user1 = new User() { Name = name };
-                            userProcessingService.UpdateUser(user1);
-                        }
-                        break;
-
-                    case "0": break;
-
-                    default:
-                        Console.WriteLine("You entered wrong input, Try again");
-                        break;
+                    }
+                    catch (UnauthorizedAccessException unAuthTop)
+                    {
+                        Console.WriteLine($"{unAuthTop.Message}");
+                    }
                 }
-            }
-            while (userChoice != "0");
 
-            Console.Clear();
-            Console.WriteLine("The app has been finished");
+                foreach (DirectoryInfo directoryInfo in root.EnumerateDirectories("*"))
+                {
+                    try
+                    {
+                        foreach (FileInfo fileInfo in directoryInfo.EnumerateFiles("*", SearchOption.AllDirectories))
+                        {
+                            try
+                            {
+                                if (fileInfo.Length >= 0)
+                                {
+                                    Console.WriteLine($"{fileInfo.FullName}\t\t{fileInfo.Length.ToString("N0")}");
+                                    totalSize += fileInfo.Length;
+                                }
+                            }
+                            catch (UnauthorizedAccessException unAuthFile)
+                            {
+                                Console.WriteLine($"unAuthFile: {unAuthFile.Message}");
+                            }
+                        }
+                    }
+                    catch (UnauthorizedAccessException unAuthSubDir)
+                    {
+                        Console.WriteLine($"unAuthSubDir: {unAuthSubDir.Message}");
+                    }
+                }
+                Console.WriteLine("Total size of files " + totalSize);
+            }
+            catch (DirectoryNotFoundException dirNotFound)
+            {
+                Console.WriteLine($"{dirNotFound.Message}");
+            }
+            catch (UnauthorizedAccessException unAuthDir)
+            {
+                Console.WriteLine($"unAuthDir: {unAuthDir.Message}");
+            }
+            catch (PathTooLongException longPath)
+            {
+                Console.WriteLine($"{longPath.Message}");
+            }
+
+            //string userChoice;
+            //UserProcessingService userProcessingService = RegisterUserProcessingService();
+
+            //do
+            //{
+            //    PrintMenu();
+            //    Console.Write("Enter your choice:");
+            //    userChoice = Console.ReadLine();
+            //    switch (userChoice)
+            //    {
+            //        case "1":
+            //            Console.Clear();
+            //            Console.Write("Enter you name:");
+            //            string userName = Console.ReadLine();
+            //            User user = new User() { Name = userName };
+            //            userProcessingService.CreateNewUser(user);
+            //            break;
+
+            //        case "2":
+            //            {
+            //                Console.Clear();
+            //                userProcessingService.DisplayUsers();
+            //            }
+            //            break;
+
+            //        case "3":
+            //            {
+            //                Console.Clear();
+            //                Console.WriteLine("Enter an id which you want to delete");
+            //                Console.Write("Enter id:");
+            //                string deleteWithIdStr = Console.ReadLine();
+            //                int deleteWithId = Convert.ToInt32(deleteWithIdStr);
+            //                userProcessingService.DeleteUser(deleteWithId);
+            //            }
+            //            break;
+
+            //        case "4":
+            //            {
+            //                Console.Clear();
+            //                Console.WriteLine("Enter an id which you want  to edit");
+            //                Console.Write("Enter an id:");
+            //                string idStr = Console.ReadLine();
+            //                int id = Convert.ToInt32(idStr);
+            //                Console.Write("Enter name:");
+            //                string name = Console.ReadLine();
+            //                User user1 = new User() { Name = name };
+            //                userProcessingService.UpdateUser(user1);
+            //            }
+            //            break;
+
+            //        case "0": break;
+
+            //        default:
+            //            Console.WriteLine("You entered wrong input, Try again");
+            //            break;
+            //    }
+            //}
+            //while (userChoice != "0");
+
+            //Console.Clear();
+            //Console.WriteLine("The app has been finished");
         }
 
         private static UserProcessingService RegisterUserProcessingService()
@@ -83,7 +145,7 @@ namespace FileDB.App
             IUserService userService = new UserService(loggingBroker, storageBroker);
             IdentityService identitiyService = IdentityService.GetInstance(storageBroker);
 
-            UserProcessingService userProcessingService = 
+            UserProcessingService userProcessingService =
                 new UserProcessingService(userService,
                         identitiyService);
 
